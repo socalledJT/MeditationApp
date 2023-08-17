1a321e6601fbb28aae4ce6d06ad63be57261bca2
Johnny Tm <36928647+socalledJT@users.noreply.github.com>
2023-08-17 16:36:18 +0200

    Design implementation
    Updated expo to latest version to get ready for mobile testing,.
    Implemented app design.

--- MediTimer/App.js
+++ MediTimer/App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const formatNumber = number => '0${number'.slice(-2);
+
+const getRemaining = (time) => {
+  const mins = Math.floor(time / 60);
+  const secs = time - mins * 60;
+  return { mins: formatNumber(mins), secs: formatNumber(secs)};
+}
 
 export default function App() {
+
+  const [remainingSecs, setRemainingSecs] = useState(0);
+  const [isActive, setisActive] = useState(false);
+  const {mins, secs} = getRemaining(setRemainingSecs);
+
+  const toggle = () => {
+    setisActive(!isActive);
+  }
+
+  useEffect = (() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setRemainingSecs(remainingSecs => remainingSecs + 1);
+      }, 1000)
+    } else if (!isActive && remainingSecs !== 0) {
+        clearInterval(interval);
+    }
+
+    return () => clearImmediate(interval);
+  }, [isActive, setRemainingSecs]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light-content" />
+      <Text style={styles.timerText}>{'${mins}:${secs}'}</Text>
+      <TouchableOpacity onPress={toggle} style={styles.button}>
+        <Text style={styles.buttonText}>{isActive ? 'Pause' : 'Start'}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +48,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2A8D7E',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: 150,
+    height: 70,
+    justifyContent: 'center',
+    backgroundColor: '#112522',
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 30,
+    color: '#fff',
+    textAlign: 'center'
+  },
+  timerText: {
+    color: '#fff',
+    fontSize: 90,
+    marginBottom: 20
+  }
 });
